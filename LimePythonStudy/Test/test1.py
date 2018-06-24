8431987f6a95a4a849588df1c92c50152ede2150
sungjae <odobenuseKR@gmail.com>
2018-06-24 20:33:06 +0900

    Lime Python Study

---
+++ LimePythonStudy/Test/test1.py
@@ -0,0 +1,5 @@
+a = [4, 2, 3, 4, 5]
+b = 3
+
+for i in range(b):
+    print(a[i])
---
+++ LimePythonStudy/Test/test2.py
@@ -0,0 +1,5 @@
+mystring = 'abcdef'
+strlen = len(mystring)
+
+answer = mystring[strlen/2 - 1] + mystring[strlen/2]
+print answer
