6ffee08e1ad697076670e42d30cccd9752d326ed
sungjae <odobenuseKR@gmail.com>
2018-06-28 19:57:40 +0900

    20180628

--- LimePythonStudy/ProgrammersAlgorithm/Level2/최댓값과 최솟값.py
+++ LimePythonStudy/ProgrammersAlgorithm/Level2/최댓값과 최솟값.py
@@ -4,5 +4,5 @@ def solution(s):
     answer_list = []
     for i in s_list:
         answer_list.append(int(i))
-    answer = str(min(answer_list)) + ' ' + str(max(answer_list))
+    answer = str(min(answer_list)) + " " + str(max(answer_list))
     return answer
